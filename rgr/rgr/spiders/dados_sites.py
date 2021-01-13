c8462ed1cd03659e280d3c89bc9a8901b4fc3209
Luciano Martins <zicadopv@gmail.com>
2021-01-13 12:09:18 -0300

    Finalizando as atualizações do Scrapy

--- rgr/rgr/spiders/dados_sites.py
+++ rgr/rgr/spiders/dados_sites.py
@@ -2,29 +2,27 @@
 from scrapy import Spider
 import re
 from bs4 import BeautifulSoup as bs
-
-"""
-para "aliviar" pros servidores usar:
-```scrapy runspider dados_sites.py -s HTTPCACHE_ENABLED=1 -o <nome-de-saida>.csv ```
-```scrapy runspider dados_sites.py -s HTTPCACHE_ENABLED=1 -s CLOSESPIDER ITEMCOUNT=500 -o <nome-de-saida>.csv ```
-"""
+from rgr.items import RgrItem
 
 
 class DadosSpider(Spider):
     name = 'dados'
-    filename = 'websites.txt'
-    start_urls = []
-    with open(filename, encoding='utf-8') as _f:
-        urls = _f.read()
-    for url in urls.split('\n'):
-        start_urls.append(url)
+    # filename = '../../websites.txt'
+    start_urls = [
+        'https://www.imobiliariasistema.com.br/'
+    ]
+    # with open(filename, encoding='utf-8') as _f:
+    #     urls = _f.read()
+    # for url in urls.split('\n'):
+    #     start_urls.append(url)
+
 
     def parse(self, response):
         html_to_texto = response.text
         search = ['img', 'src', 'logo']
         pattern = '(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})'
         src = ''
-        telefone = ''
+        lista_telefone = []
         for linha in html_to_texto.split('\n'):
             res = [ele for ele in search if (ele in linha)]
             matchs = re.search(pattern, linha)
@@ -44,12 +42,14 @@ class DadosSpider(Spider):
             if '0800 ' in linha:
                 telefone = re.sub('[^0-9]', ' ', linha)
                 telefone = telefone.strip()
+                lista_telefone.append(telefone)
             if matchs:
-                if 'telep' in linha or 'telef' in linha:
-                    telefone = re.sub('[^0-9]', ' ', linha)
-                    telefone = telefone.strip()
-            yield {
-                'website': response.url,
-                'telefone': telefone,
-                'logo': src
-            }
+                # if 'telep' in linha or 'telef' in linha:
+                telefone = re.sub('[^0-9]', ' ', linha)
+                telefone = telefone.strip()
+                lista_telefone.append(telefone)
+        yield RgrItem(
+            website=response.url,
+            telefone=lista_telefone,
+            logo=src
+        )
